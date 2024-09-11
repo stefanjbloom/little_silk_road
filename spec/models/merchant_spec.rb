df01c7e72f2feaf6aedb661f3e06fd5ceb0deb07
Karl Fallenius <smilodon.p@gmail.com>
2024-09-11 16:27:43 -0600

    adds relationship testing to model tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,7 +2,8 @@ require "rails_helper"
 
 RSpec.describe Merchant, type: :model do
   describe 'relationships' do
-    
+    it { should have_many :items }
+    it { should have_many :invoices }
   end
 
 end
