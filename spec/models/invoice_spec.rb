d42e8678715468c9baae4bf46df59438597877c5
Karl Fallenius <smilodon.p@gmail.com>
2024-09-11 16:35:26 -0600

    refactors relationship testing in model tests

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe Invoice, type: :model do
     it { should belong_to(:customer) }
     it { should have_many(:transactions) }
     it { should have_many(:invoice_items) }
-    it { should have_many(:items).through():invoice_items) }
+    it { should have_many(:items).through(:invoice_items) }
   end
 end
