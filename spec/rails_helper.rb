3d076bddd82d1fa2f53a004826d1abc3107be153
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-09-11 13:48:47 -0600

    test: retrieve all merchants

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,7 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  config.formatter = :documentation 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
