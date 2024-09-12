b3dbb3ca0e03e2f30a4629c339d05d1c4a926ee9
Stefan Bloom <stefanjbloom88@gmail.com>
2024-09-11 19:33:34 -0600

    Feat: incomplete validation work for models for a SORT function.  will continue tomorrow

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
 	belongs_to :merchant
 	has_many :invoice_items
 	has_many :invoices
+
+	validates :name, :description, :unit_price, :merchant_id {message: "is required"}
+	validates :name, :description, :unit_price, comparison: {other_than: "", message: "cannot be blank"}
+	validates :unit_price, numericality: {message: "should be a valid price"}
 end
