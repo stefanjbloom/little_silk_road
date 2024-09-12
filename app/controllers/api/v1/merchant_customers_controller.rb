10d9874c9b608e99d35a59bcf610c94b23d872f3
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-09-11 21:36:52 -0600

    refactor: cleaned up commented pry sessions, whitespace

--- app/controllers/api/v1/merchant_customers_controller.rb
+++ app/controllers/api/v1/merchant_customers_controller.rb
@@ -1,10 +1,8 @@
 class Api::V1::MerchantCustomersController < ApplicationController
-
+  
   def index
     merchant = Merchant.find(params[:id])
     customers = merchant.customers
     render json: CustomerSerializer.new(customers)
-    # require 'pry'; binding.pry
   end
-
 end
