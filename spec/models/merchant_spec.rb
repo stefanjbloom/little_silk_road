1d0fc6993eb132153797bc957298c9856c673f7d
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-09-12 18:53:12 -0600

    Test: validations merchant spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Merchant, type: :model do
     it { should have_many (:invoices) }
   end
 
+  describe 'validations' do
+    it { should validate_presence_of(:name).with_message("Please enter a merchant name") }
+    it { should validate_uniqueness_of(:name).with_message("This merchant has already been created")}
+  end
+
 end
