92e014b841f04070ed2754f5ec740fa10394d502
Stefan Bloom <stefanjbloom88@gmail.com>
2024-09-15 16:38:33 -0600

    Feat: implemented rescue in def index to render safe json response of 404 item not found

--- app/controllers/api/v1/items_merchant_controller.rb
+++ app/controllers/api/v1/items_merchant_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::ItemsMerchantController < ApplicationController
   
   def index
-    item = Item.find(params[:id])
-    merchant = item.merchant
-    render json: MerchantSerializer.new(merchant)
+    begin
+      item = Item.find(params[:id])
+      merchant = item.merchant
+      render json: MerchantSerializer.new(merchant)
+    rescue ActiveRecord::RecordNotFound
+      render json: {error: "404: Item Not Found"}, status: 404
+    end
   end
 end
 # return the merchant associated with an item
