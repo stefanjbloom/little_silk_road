ac221c1a74cb7238e0a991f83cf0177ea585e678
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-09-15 18:24:19 -0600

    Refactor: changed response body to match assignment

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -2,12 +2,26 @@ class ErrorSerializer
   # include JSONAPI::Serializer
   def self.format_error(exception, status)
     {
-      errors: [
-        {
-          status: status,
-          title: exception.message
-        }
-      ]
+      message: 'We could not complete your request, please enter new query.',
+      errors: [exception.message]
     }
   end
 end
+
+# NOTE: For when we reference this project in the future.
+
+# below is properly the formatted error response for JSON convention.
+# error should be the only top level key.
+
+# class ErrorSerializer
+#   def self.format_error(exception, status)
+#     {
+#       errors: [
+#         {
+#           status: status,
+#           title: exception.message
+#         }
+#       ]
+#     }
+#   end
+# end
