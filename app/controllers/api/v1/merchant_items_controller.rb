7717560d51fcf45d5f23e6e76430a8d48eda4056
Stefan Bloom <stefanjbloom88@gmail.com>
2024-09-16 13:23:18 -0600

    Feat/Test: customer model validations and tests green

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::MerchantItemsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
   
   def index
     merchant = Merchant.find(params[:id])
