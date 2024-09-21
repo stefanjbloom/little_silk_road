f03db14f568ec77cf6894c8c4442799cd577fe21
Stefan Bloom <stefanjbloom88@gmail.com>
2024-09-20 19:16:11 -0600

    Feat: route and controller action #index created and working

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -10,4 +10,10 @@ class Api::V1::CouponsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render json: { error: "Coupon not found" }, status: :not_found
   end
+
+  def index
+    merchant = Merchant.find(params[:merchant_id])
+    coupons = merchant.coupons
+    render json: CouponSerializer.new(coupons)
+  end
 end
